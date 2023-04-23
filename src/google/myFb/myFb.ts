a5592d9edcb53d446a91b7a625510c5590d9b0b0
Matthew Wen <mattwen2018@gmail.com>
2023-04-22 22:33:47 -0500

    Enable Project to be display on Depricated Website

--- src/google/myFb/myFb.ts
+++ src/google/myFb/myFb.ts
@@ -1,6 +1,5 @@
 import {applicationDefault, initializeApp} from 'firebase-admin/app';
 import {App} from "firebase-admin/lib/app/core";
-import {credential} from "firebase-admin";
 
 export class MyFirebase {
 
@@ -9,12 +8,7 @@ export class MyFirebase {
 
     constructor() {
         if (MyFirebase.app === undefined) {
-            if (process.env.GOOGLE_APPLICATION_JSON) {
-                MyFirebase.appDefault = credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_JSON));
-            }
-            else {
-                MyFirebase.appDefault = applicationDefault();
-            }
+            MyFirebase.appDefault = applicationDefault();
             MyFirebase.app = initializeApp({
                 credential: MyFirebase.appDefault
             });
